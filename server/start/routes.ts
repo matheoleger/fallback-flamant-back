da79fd39a438539b3437aeefba4727aff38e585b
Flavio <flavio.cormerais@ynov.com>
2023-02-07 09:30:25 +0100

    ajout cart

--- server/start/routes.ts
+++ server/start/routes.ts
@@ -20,9 +20,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get('/cart', 'CartsController.getAllUsers')
 
 Route.group(() => {
   Route.get('/', 'UsersController.getAllUsers')
